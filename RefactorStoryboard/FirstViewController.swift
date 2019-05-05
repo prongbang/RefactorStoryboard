1798d8fe5c7bf4159a2ab91f1a537c42fe067534
prongbang <prongbang@gmail.com>
2019-05-05 15:04:19 +0700

    Initial Commit

---
+++ RefactorStoryboard/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RefactorStoryboard
+//
+//  Created by prongbang on 5/5/2562 BE.
+//  Copyright © 2562 prongbang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ RefactorStoryboard/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RefactorStoryboard
+//
+//  Created by prongbang on 5/5/2562 BE.
+//  Copyright © 2562 prongbang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
